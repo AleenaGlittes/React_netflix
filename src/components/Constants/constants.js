31071c895e2d5d3446f528cebb4beae93a9503fa
Aleena Vincent <aleenavincent4u@gmail.com>
2023-08-03 09:29:11 +0530

    Initial commit

---
+++ src/components/Constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl='https://api.themoviedb.org/3';
+export const API_KEY='7753bbd213c9b2909e8f80e9f726d82c'
+export const imageUrl ='https://image.tmdb.org/t/p/original';
---
+++ src/urls.js
@@ -0,0 +1,4 @@
+import { API_KEY} from './components/Constants/constants'
+
+export const originals=`tv/top_rated?api_key=${API_KEY}&language=en-US`
+export const action=`discover/movie?api_key=${API_KEY}&with_genres=28`
